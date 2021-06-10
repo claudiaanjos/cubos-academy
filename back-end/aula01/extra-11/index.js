fd0659e570232beae53fd2ab613afbac3faf81d6
Claudia <ndosanjosc@gmail.com>
2021-06-10 20:19:19 -0300

    exercicio

--- back-end/aula01/extra-11/index.js
+++ back-end/aula01/extra-11/index.js
@@ -1,5 +1,5 @@
 function calcula (diametro){
-    let raio = Math.sqrt(diametro)
+    let raio = diametro/2
     let volume = (4/3)*Math.PI*(raio**3)
     return `O volume de uma esfera de raio ${raio} é ${volume}.`
 }
