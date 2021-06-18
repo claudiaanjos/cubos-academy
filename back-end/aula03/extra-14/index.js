cb36fe3557a907fdfc1af5f30107d8e3c23b4b4a
Claudia <ndosanjosc@gmail.com>
2021-06-18 16:09:30 -0300

    exercicios

--- back-end/aula03/extra-14/index.js
+++ back-end/aula03/extra-14/index.js
@@ -1,3 +1,17 @@
-const arrayA = [5, 32, 3, 44, 1];
-const arrayB = [57, 4, 21, 2, 13];
+const arrayA = [5, 32, 3, 44, 1]
+const arrayB = [57, 4, 21, 2, 13]
+
+for (let i = 0; i < arrayA.length(); i++){
+    if (arrayA[i] <= arrayB[i]) {
+        console.log(arrayA[i])
+    } else {
+        console.log(arrayB[i])
+    }
+}
+
+
+
+
+
+
 
