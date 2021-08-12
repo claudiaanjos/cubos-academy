58ddf7321655cf88f85b08e7fbebd8868633d85f
Claudia <ndosanjosc@gmail.com>
2021-08-12 12:42:09 -0300

    exercicios

--- back-end/aula13/classe-01/controladores/pokemon.js
+++ back-end/aula13/classe-01/controladores/pokemon.js
@@ -4,5 +4,35 @@ async function obterPokemons (req, res) {
     let inicio = req.query.inicio;
     let quantidade = req.query.quantidade;
 
+    if (!inicio) {
+        inicio = 0;
+    }
+    if (!quantidade) {
+        quantidade = 20;
+    }
 
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${inicio}&limit=${quantidade}`);
+
+    res.json(response.data.results);
 }
+
+async function obterPokemon(req, res) {
+    const idOuNome = req.params.idOuNome;
+
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idOuNome}`);
+
+    const { id, name, height, weight, base_experience, forms, abilities, species } = response.data;
+
+    res.json({
+        id,
+        name,
+        height,
+        weight,
+        base_experience,
+        forms,
+        abilities,
+        species
+    });
+}
+
+module.exports = { obterPokemons, obterPokemon };
