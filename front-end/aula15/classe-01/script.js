8518aeb76ded9f9a06b1deddf409c6a58f465daf
Claudia <ndosanjosc@gmail.com>
2021-08-17 21:00:27 -0300

    exercicios

--- front-end/aula15/classe-01/script.js
+++ front-end/aula15/classe-01/script.js
@@ -1,7 +1,39 @@
+const paises = document.querySelector('.paises');
+const input = document.querySelector('.busca');
+
+
 fetch('https://restcountries.eu/rest/v2/all').then(function(resposta) {
     const promiseBody = resposta.json();
     
     promiseBody.then(function(body) {
-        console.log(body);
+        body.forEach(function(item) {
+            const pais = document.createElement('div');
+            pais.classList.add('pais');
+
+            const nome = document.createElement('h2');
+            nome.textContent = item.name;
+
+            const regiao = document.createElement('span');
+            regiao.textContent = "Região: " + item.region + " ";
+
+            const capital = document.createElement('span');
+            capital.textContent = "Capital: " + item.capital;
+
+            const populacao = document.createElement('p');
+            populacao.textContent = "População: " + item.population;
+
+            const bandeira = document.createElement('img');
+            bandeira.src = item.flag;
+            bandeira.alt = item.name;
+
+            pais.append(nome, regiao, capital, populacao, bandeira);
+            paises.append(pais);
+        });
     });
+});
+
+input.addEventListener('keydown', function(event) {
+    if (event.key !== 'Enter' || input.value === '') return;
+    
+
 });
