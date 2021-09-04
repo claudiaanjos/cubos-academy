969f2b897cf1617ecdd7a721a7b232041ad43985
Claudia <ndosanjosc@gmail.com>
2021-09-03 21:33:46 -0300

    exercicios

--- front-end/aula17/base/classe-02/src/App.js
+++ front-end/aula17/base/classe-02/src/App.js
@@ -1,18 +1,74 @@
-import close from './assets/close.svg';
-import cookie from './assets/cookie.svg';
+import kelvin from './assets/kelvin-costa.png';
+import mario from './assets/mario-hisashi.png';
+import anna from './assets/anna-bia.png';
+import charles from './assets/charles-santos.png';
+
+
+function UserCard({ picture, name, username, followers, following}) {
+  return (
+    <div className="card">
+      <img src={picture} alt={name} />
+      <h2>{name}</h2>
+      <span className="username">
+        {username}
+      </span>
+      <span className="stats">
+        {followers} seguidores <br />
+        {following} seguindo
+      </span>
+    </div>
+  )
+}
 
 
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      name: "Kelvin Costa",
+      picture: kelvin,
+      username: "@costa",
+      followers: 140,
+      following: 207
+    },
+    {
+      id: 2,
+      name: "Charles Santos",
+      picture: charles,
+      username: "@charles.santos",
+      followers: 302,
+      following: 419
+    },
+    {
+      id: 3,
+      name: "Anna Bia",
+      picture: anna,
+      username: "@anab",
+      followers: 842,
+      following: 150
+    },
+    {
+      id: 4,
+      name: "Mario Hisashi",
+      picture: mario,
+      username: "@hisashi",
+      followers: 28,
+      following: 17
+    }
+  ];
+
   return (
     <div className="App">
-      <div className="card">
-        <img src={close} alt="close" className="close-button" />
-        <img src={cookie} alt="cookie" />
-        <p>Nós utilizamos cookies para melhorar nossa UX, analytics e marketing</p>
-        <button>
-          Tudo bem!
-        </button>
-      </div>
+
+      {users.map(function (user) {
+        return ( 
+        <UserCard
+        key={user.id}
+          {...user} />
+        )
+      })}
+
     </div>
   );
 }
