fc55cd83cbeed5e0217bbce13c3d73441c1907d3
Claudia <ndosanjosc@gmail.com>
2021-09-15 11:49:35 -0300

    exercicios

---
+++ back-end/aula18/README.md
@@ -0,0 +1,16 @@
+![](https://i.imgur.com/xG74tOh.png)
+
+## Exercícios de classe 🏫
+
+1. Modelando e criando Banco de dados
+
+---
+
+Preencha a checklist para fazer os exercícios:
+
+-   [ ] Fazer o fork do repositório para sua conta
+-   [ ] Executar `git clone` do seu fork no terminal para clonar o repositório, ou clonar de outra maneira
+-   [ ] Após fazer e commitar todos os exercícios fazer o `git push` para seu fork
+-   [ ] Copiar a url do seu fork e enviar na plataforma
+
+###### tags: `módulo 3` `exercício de classe` `banco de dados` `nodeJS` `SQL` `postgresql`
--- front-end/base/src/App.js
+++ front-end/base/src/App.js
@@ -1,9 +1,68 @@
+import { useState } from 'react';
+
+
+function Gerenciador(props) {
+  return (
+    <li>
+      <span
+        onClick={() => props.handleComplete(props.id)}
+        style={{ textDecoration: props.completa ? 'line-through' : '' }}
+      >
+        {props.children}
+      </span>
+      <button onClick={() => props.handleDelete(props.id)}>x</button>
+    </li>
+  )
+}
+
+
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  function handleKeyDown(event){
+    if (event.key !== 'Enter') return;
+
+    const listaTarefas = [...tarefas, { id: Math.random(), texto: event.target.value, completa: false }];
+    console.log(listaTarefas)
+    setTarefas(listaTarefas);
+
+    event.target.value = '';
+  }
+
+  function handleDelete(id){
+    const novasTarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    setTarefas(novasTarefas);
+  }
+
+  function handleComplete(id){
+    const novasTarefas = [...tarefas];
+    const tarefaCompletada = novasTarefas.find(tarefa => tarefa.id === id);
+    tarefaCompletada.completa = !tarefaCompletada.completa;
+    
+    setTarefas(novasTarefas);
+  }
+
   return (
     <div className="App">
-    
-    </div>
+       <h2>Gerenciador de Tarefas</h2>
+       <input type="text" onKeyDown={handleKeyDown} />
+       <ul>
+        {tarefas.map(function(tarefa){
+          return (
+            <Gerenciador
+              key={tarefa.id}
+              id={tarefa.id}
+              handleDelete={handleDelete}
+              handleComplete={handleComplete}
+              completa={tarefa.completa}
+            >
+              {tarefa.texto}
+            </Gerenciador>
+          );
+        })}
+       </ul>
+     </div>
   );
 }
 
