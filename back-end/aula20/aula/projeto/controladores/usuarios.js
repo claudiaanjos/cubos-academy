ae4106718b00236f2f5d3f4844c1966c50d6658c
Claudia <ndosanjosc@gmail.com>
2021-09-23 12:49:19 -0300

    exercicios

--- back-end/aula20/aula/projeto/controladores/usuarios.js
+++ back-end/aula20/aula/projeto/controladores/usuarios.js
@@ -1,8 +1,10 @@
 const conexao = require("../conexao");
 const securePassword = require("secure-password");
 const jwt = require("jsonwebtoken");
+// Vamos importar a chave secreta
 const jwtSecret = require("../jwt_secret");
 
+// Inicializar o pacote secure-password
 const pwd = securePassword()
 
 const cadastrarUsuario = async (req, res) => {
@@ -32,15 +34,17 @@ const cadastrarUsuario = async (req, res) => {
     }
 
     try {
+        // Precisamos colocar o buffer.from, pois a senha não pode ser string, mas sim um buffer. A função retorna um buffer, mas para salvar no BD preciso transformar em string e colocar numa codificação, escolhemos s o hexadecimal.
         const hash = (await pwd.hash(Buffer.from(senha))).toString("hex");
         const query = 'insert into usuarios (nome, email, senha) values ($1, $2, $3)';
+        // Guardamos no Banco de Dados o hash e não a senha
         const usuario = await conexao.query(query, [nome, email, hash]);
 
         if (usuario.rowCount === 0) {
             return res.status(400).json('Não foi possivel cadastrar o usuário');
         }
 
-        return res.status(200).json('Usuário cadastrado com sucesso.')
+        return res.status(200).json('Usuário cadastrado com sucesso.');
     } catch (error) {
         return res.status(400).json(error.message);
     }
@@ -66,7 +70,7 @@ const login = async (req, res) => {
         }
 
         const usuario = usuarios.rows[0];
-
+        // O 1º buffer é o que é passado no body e o 2º do BD
         const result = await pwd.verify(Buffer.from(senha), Buffer.from(usuario.senha, "hex"));
 
         switch (result) {
@@ -75,16 +79,18 @@ const login = async (req, res) => {
                 return res.status(400).json("Email ou senha incorretos.");
             case securePassword.VALID:
                 break;
+            //  Se tiver válido, mas precisa trocar o hash (fazer o mesmo procedimento acima)
             case securePassword.VALID_NEEDS_REHASH:
                 try {
                     const hash = (await pwd.hash(Buffer.from(senha))).toString("hex");
                     const query = 'update usuarios set senha = $1 where email = $2';
                     await conexao.query(query, [hash, email]);
+                // Se der erro, vou ignorar o erro, pois a senha está válida
                 } catch {
                 }
                 break;
         }
-
+        // Sign é uma assinatura. Temos os dados que queremos assinar e a chave (que está no arquivo jwt.secret.js)
         const token = jwt.sign({
             id: usuario.id,
             nome: usuario.nome,
