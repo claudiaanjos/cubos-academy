0b77fa0dbcfbb1a6eee71f702577b88dcdae83dd
Claudia dos Anjos <ndosanjosc@gmail.com>
2021-10-28 17:15:08 -0300

    exercicio

--- back-end/aula25/resolucao/src/conexao.js
+++ back-end/aula25/resolucao/src/conexao.js
@@ -3,8 +3,8 @@ const knex = require('knex')({
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     }
 });
 
